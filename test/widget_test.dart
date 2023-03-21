4f382d3ee74042793479b0ee1ee0cf7e25a6aad4
Jade Oliveira <jade.ufrgs@gmail.com>
2023-03-21 20:44:22 -0300

    fixed some lint warnings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_flutter_solid/main.dart';
+import 'package:test_flutter_solid/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
